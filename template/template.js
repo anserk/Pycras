52c6e344517101ef963b31984e17ed843d1708c7
anserk <rotondi.simone@gmail.com>
2015-06-03 21:10:16 -0400

    add some style to the html output

---
+++ template/template.js
@@ -0,0 +1,13 @@
+alert('hello');
+
+// // converts categories to a readable format
+// conversion_table= ['sof':'software','web':'web developer'];
+
+// function convert(category) {
+// 	return conversion_table[category];
+// }
+
+// def function asd() {
+// 	console.log('hey!');
+// 	return 'hello';
+// }
